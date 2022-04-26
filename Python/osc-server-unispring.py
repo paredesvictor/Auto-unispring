c757ad24ea842ea376fe5939cdfb7e6be954e81c
FidelJackson <95363485+paredesvictor@users.noreply.github.com>
2022-04-26 11:52:00 +0200

    clean

--- Python/osc-server-unispring.py
+++ Python/osc-server-unispring.py
@@ -1,83 +1,74 @@
-import argparse
-import sys
-from copy import deepcopy
-import unispring as usp
-from random import random
 from pythonosc import dispatcher
 from pythonosc import osc_server
 from pythonosc import udp_client
+import argparse
+import numpy as np
+import unispring as usp
 
-class recursion_depth:
-    def __init__(self, limit):
-        self.limit = limit
-        self.default_limit = sys.getrecursionlimit()
-    def __enter__(self):
-        sys.setrecursionlimit(self.limit)
-    def __exit__(self, type, value, traceback):
-        sys.setrecursionlimit(self.default_limit)
+def normalize(track):
+    n_descr = len(track['1'][0])
+    norm_track = {}
+    list_min = [float('inf') for i in range(n_descr)]
+    list_max = [float('-inf') for i in range(n_descr)]
+    for key, table in track.items():
+        for line in table:  
+            for i in range(1,n_descr):
+                if line[i] < list_min[i]:
+                    list_min[i] = line[i]
+                elif line[i] > list_max[i]:
+                    list_max[i] = line[i]
+    for key, table in track.items():
+        norm_track[key] = []
+        for line in table:
+            new_line = [line[0]]
+            for i in range(1,n_descr):
+                new_line.append((line[i]-list_min[i])/(list_max[i]-list_min[i]))
+            norm_track[key].append(new_line)
+    return norm_track
 
-def update_unispring(addrs, args, *coord):
-    print('updating...')
-    n = 1
-    while True:
-        try:
-            with recursion_depth(1000*n):
-                temp_corpus = deepcopy(args[1]["corpus1"])
-            break
-        except:
-            n += 1
-    vertices = [(coord[i],1-coord[i+1]) for i in range(0,len(coord),2)]
-    region = usp.RegionPolygon(vertices)
-    temp_corpus.region = region
-    temp_corpus.unispringUniform(1, 0.01, 0.02, limit=200*(len(vertices)/4))
-    print('export')
-    save_dir = args[1]['dir']
-    temp_corpus.exportJson(save_dir+'/remap.json')
-    args[0].send_message("/unispring", save_dir)
-    print('waiting...')
+def add_line(addrs, args, *message):
+    index = int(message[-2])
+    buffer = str(message[-1])
+    n_descr = len(message)-2
+    descriptors = [message[i] for i in range(n_descr)]
+    args[1]['buffer'][buffer][index] = descriptors
+    #args[0].send_message('/callback', truc)
 
-def update_unispring_rand(addrs, args, *coord):
-    print('random update...')
-    n = 1
-    while True:
-        try:
-            with recursion_depth(1000*n):
-                temp_corpus = deepcopy(args[1]["corpus1"])
-            break
-        except:
-            n += 1
-    pt1 = (random()*0.5,random()*0.5)
-    pt2 = (random()*0.5+0.5,random()*0.5)
-    pt3 = (random()*0.5+0.5,random()*0.5+0.5)
-    pt4 = (random()*0.5,random()*0.5+0.5)
-    vertices = [pt1, pt2, pt3, pt4]
-    region = usp.RegionPolygon(vertices)
-    temp_corpus.region = region
-    temp_corpus.unispringUniform(1, 0.01, 0.02, limit=200*(len(vertices)/4))
-    print('export')
-    save_dir = args[1]['dir']
-    temp_corpus.exportJson(save_dir+'/remap.json')
-    args[0].send_message("/unispring", save_dir)
-    print('waiting...')
+def add_buffer(addrs, args, *message):
+    n_cols = int(message[2])
+    n_rows = int(message[0])
+    buffer = str(message[1])
+    args[1]['buffer'][buffer] = [[] for j in range(n_rows)]
 
-def init_unispring(addrs, args, max_dir):
-    directory = '/' + max_dir.replace(' ','')
-    print('Creating corpus and region')
+def eval_str(addrs, args, eval_string):
+    eval(eval_string)
+
+def reset_track(addrs, args, *unused):
+    args[1]['buffer'] = {}
+
+def dumpdone(addrs, args, buffer):
+    n_lines = len(args[1]['buffer'][str(buffer)])
+    print('done importing buffer ', buffer, ', ',n_lines, ' grains.' )
+
+def create_norm_track(addrs, args, *unused):
+    args[1]['norm_buffer'] = {}
+    args[1]['norm_buffer'] = normalize(args[1]['buffer'])
+    print('done normalizing')
+
+def write_norm_track(addrs, args, *unused):
+    for idx_buffer, track in args[1]['norm_buffer'].items():
+        args[0].send_message('/buffer_index', int(idx_buffer))
+        for i,line in enumerate(track):
+            args[0].send_message('/append', line)
+    print('done exporting normalized buffers')
+
+def unispring(addrs, args, *descr):
     vertices = ((0,0),(1,0),(1,1),(0,1))
-    descX = "CentroidMean"
-    descY = "PeriodicityMean"
     region = usp.RegionPolygon(vertices)
-    corpus = usp.Corpus(directory + '/corpus.json',
-    region, descX, descY, plot=False)
-    print('uniformization...')
-    limit = min(1000, max(200, len(corpus.getAllPoints())))
-    corpus.unispringUniform(1, 0.01, 0.02, limit=limit)
-    print('export')
-    corpus.exportJson(directory + '/remap.json')
-    args[1]['corpus1'] = corpus
-    args[1]['dir'] = directory
-    args[0].send_message("/unispring", directory)
-    print('waiting...')
+    args[1]['corpus'] = usp.Corpus(args[1]['norm_buffer'], region, descr[0], descr[1])
+    args[1]['corpus'].unispringUniform(1, 0.01, 0.02, exportPeriod=0, client=args[0])
+    print('uniformization done')
+    args[1]['corpus'].exportToMax(args[0])
 
 if __name__ == "__main__":
     parser_client = argparse.ArgumentParser()
@@ -87,17 +78,22 @@ if __name__ == "__main__":
 
     client = udp_client.SimpleUDPClient(args_client.ip, args_client.port)
     
-    
     parser_server = argparse.ArgumentParser()
     parser_server.add_argument("--ip", default="127.0.0.1")
     parser_server.add_argument("--port", type=int, default=8011)
     args_server = parser_server.parse_args()
     
-    corpus = {}
+    global_hash = {'buffer':{}}
     dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/region_rand", update_unispring_rand, client, corpus)
-    dispatcher.map("/region", update_unispring, client, corpus)
-    dispatcher.map("/unispring", init_unispring, client, corpus)
+    dispatcher.map("/add_line", add_line, client, global_hash)
+    dispatcher.map("/add_buffer", add_buffer, client, global_hash)
+    dispatcher.map("/create_norm_track", create_norm_track, client, global_hash)
+    dispatcher.map("/write_norm_track", write_norm_track, client, global_hash)
+    dispatcher.map("/reset_track", reset_track, client, global_hash)
+    dispatcher.map("/dumpdone", dumpdone, client, global_hash)
+    dispatcher.map("/unispring", unispring, client, global_hash)
+    dispatcher.map("/print", print)
+    dispatcher.map("/eval", eval_str, client, global_hash)
     
     server = osc_server.ThreadingOSCUDPServer(
         (args_server.ip, args_server.port), dispatcher)
